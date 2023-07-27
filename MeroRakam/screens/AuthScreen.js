638c93ddbf1b3e9c777dc0fe2e5e560f3171655f
sushilbhattarai45 <bhattaraisushil005@gmail.com>
2023-07-27 16:43:13 +0545

    auth starting

---
+++ MeroRakam/screens/AuthScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text, StyleSheet, ScrollView, Pressable } from "react-native";
+import Colors from "../components/styles";
+import { MaterialIcons } from "@expo/vector-icons";
+
+export default function Auth({}) {
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
+}
